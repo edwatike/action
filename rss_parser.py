4af34e2dab44da5cd6289f995ac660ab5226a5cb
edwatike <edwatik@gmail.com>
2025-03-26 22:57:41 +0300

    Update rss_parser.py

--- rss_parser.py
+++ rss_parser.py
@@ -7,6 +7,8 @@ from datetime import datetime
 import hashlib
 import logging
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 # Настройка логирования
@@ -17,7 +19,7 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 # Настройки
 rss_urls = [
